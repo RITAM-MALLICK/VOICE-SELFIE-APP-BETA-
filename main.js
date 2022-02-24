2276e746271f6b126beebb7a0345931ee15641c8
RITAM-MALLICK <87016544+RITAM-MALLICK@users.noreply.github.com>
2022-02-24 16:27:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+function start() { 
+    document.getElementById("textBox").innerHTML = "";
+    recognition.start(); 
+}
+recognition.onresult = function(event) {
+     console.log(event);
+     var Content = event.results[0][0].transcript; 
+     document.getElementById("texBox").innerHTML = Content;
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = document.getElementById("textBox").value;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth_speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width : 360,
+    height : 250,
+    image_format : 'png',
+    png_quality : 90
+});
+
+camera = document.getElementById("camera");
